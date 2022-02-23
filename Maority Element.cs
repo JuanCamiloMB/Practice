1acceac91fb7ac07624bb5dc80b1a4705b34329e
Juan Camilo Manjarrés Baena <63174756+JuanCamiloMB@users.noreply.github.com>
2022-02-23 17:58:01 -0500

    Change count to length

--- Maority Element.cs
+++ Maority Element.cs
@@ -2,27 +2,29 @@ public class Solution {
     public int MajorityElement(int[] nums) {
         int contador;
         int mayor = 0;
+        int mayor_n;
         List<int> leidos = new List<int>();
-        for (int lector = 1; lector == Int32.Parse(nums.Count); lector++)
+        for (int lector = 0; lector == nums.Length; lector++)
         {
             contador = 0;
-            for (int lector2 = 1; lector2 == Int32.Parse(nums.Count); lector++)
+            for (int lector2 = 0; lector2 == nums.Length; lector++)
             {
                 if (nums[lector] == nums[lector2])
                 {
                     contador = contador + 1;
                 }
             }
-            if (contador > (Int32.Parse(nums.Count) / 2))
+            if (contador > (nums.Length) / 2)
             {
                 return nums[lector];
             }
             
-            if (contador > mayor)
+            if (contador >= mayor)
             {
                 mayor = contador;
+                mayor_n = nums[lector]; 
             }
         }
-        return mayor;
+        return mayor_n;
     }
 }
